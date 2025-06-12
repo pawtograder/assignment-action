b8ce818f0ed3bfad2a61595c504e0e52c11b7389
Smaran-Teja-P <smaranteja32@gmail.com>
2025-06-12 00:05:56 -0400

    script builder independent of python, full shell-ified

--- src/grading/builders/ScriptBuilder.ts
+++ src/grading/builders/ScriptBuilder.ts
@@ -27,15 +27,21 @@ async function parseMutationResultsFromJson(
 }
 export default class ScriptBuilder extends Builder {
   async installDependencies(): Promise<void> {
-    await this.executeCommandAndGetOutput(
-      'pip',
-      ['install', '-r', 'requirements.txt'],
+    const { returnCode, output } = await this.executeCommandAndGetOutput(
+      './install_dependencies.sh',
+      [],
       this.logger
     )
+    if (returnCode !== 0) {
+      throw new Error(
+        `Unable to invoke dependency installation script. Here is the output that was produced on the grading server 
+        when trying to install dependencies: ${output}`
+      )
+    }
   }
   async lint(): Promise<LintResult> {
     await this.installDependencies()
-    this.logger.log('hidden', 'Linting with Pylint')
+    this.logger.log('hidden', 'Generating linting reports with provided script')
     const { returnCode, output } = await this.executeCommandAndGetOutput(
       './generate_linting_reports.sh',
       [],
@@ -43,7 +49,7 @@ export default class ScriptBuilder extends Builder {
     )
     if (returnCode !== 0) {
       throw new Error(
-        `Unable to invoke pylint linting task. Here is the output that was produced on the grading server 
+        `Unable to invoke linting script. Here is the output that was produced on the grading server 
         when trying to generate linting reports: ${output}`
       )
     }
@@ -54,7 +60,7 @@ export default class ScriptBuilder extends Builder {
   }
   async getCoverageReport(): Promise<string> {
     await this.installDependencies()
-    this.logger.log('hidden', 'Generating python coverage report')
+    this.logger.log('hidden', 'Generating coverage report with provided script')
 
     // Script to generate html coverage report
     await this.executeCommandAndGetOutput(
@@ -70,10 +76,10 @@ export default class ScriptBuilder extends Builder {
       this.logger
     )
 
-    if (returnCode !== 0 || !output) {
+    if (returnCode !== 0) {
       throw new Error(
         `Unable to generate coverage report. Here is the output that was produced on the grading server 
-        when trying to generate coverage reports: ${output ? output : '[no output was produced]'}`
+        when trying to generate coverage reports: ${output}`
       )
     }
 
@@ -84,21 +90,22 @@ export default class ScriptBuilder extends Builder {
   }
   async test({ timeoutSeconds }: BuildStepOptions): Promise<TestResult[]> {
     await this.installDependencies()
-    this.logger.log('hidden', 'Unittesting with Python')
+    this.logger.log('hidden', 'Running tests with provided script')
 
-    const { returnCode } = await this.executeCommandAndGetOutput(
-      'python3',
-      ['test_runner.py'],
+    const { returnCode, output } = await this.executeCommandAndGetOutput(
+      './test_runner.sh',
+      [],
       this.logger,
       timeoutSeconds,
       true
     )
     if (returnCode !== 0) {
-      this.logger.log(
-        'hidden',
-        `python test execution failed, return code: ${returnCode}`
+      throw new Error(
+        `Unable to invoke test runner script. Here is the output that was produced on the grading server 
+        when trying to run tests: ${output}`
       )
     }
+    //Note: This method assumes that test_runner.sh generates xml files containing checkstyle compatible test results
     return await processXMLResults(
       `${this.gradingDir}/test_results/*.xml`,
       this.logger
@@ -108,21 +115,23 @@ export default class ScriptBuilder extends Builder {
     timeoutSeconds
   }: BuildStepOptions): Promise<MutantResult[]> {
     await this.installDependencies()
-    this.logger.log('hidden', 'Unittesting with Python')
+    this.logger.log('hidden', 'Running mutation tests with provided script')
 
-    const { returnCode } = await this.executeCommandAndGetOutput(
-      'python3',
-      ['mutation_test_runner.py'],
+    const { returnCode, output } = await this.executeCommandAndGetOutput(
+      './mutation_test_runner.sh',
+      [],
       this.logger,
       timeoutSeconds,
       true
     )
     if (returnCode !== 0) {
-      this.logger.log(
-        'hidden',
-        `Mutation test execution failed, return code: ${returnCode}`
+      throw new Error(
+        `Unable to invoke mutation testing script. Here is the output that was produced on the grading server 
+        when trying to run mutation tests: ${output}`
       )
     }
+    //Note: This method assumes that mutation_test_runner.sh generates JSON files containing MutantResults
+    // in the mutation_test_results directory.
     return await parseMutationResultsFromJson(
       `${this.gradingDir}/mutation_test_results/*.json`,
       this.logger
@@ -132,14 +141,6 @@ export default class ScriptBuilder extends Builder {
     await this.executeCommandAndGetOutput(
       'rm',
       ['-rf', 'test_results', 'coverage_reports', 'mutation_test_results'],
-      this.logger,
-      timeoutSeconds,
-      true
-    )
-
-    await this.executeCommandAndGetOutput(
-      'rm',
-      ['-r', '.coverage'],
       this.logger,
       timeoutSeconds,
       true
