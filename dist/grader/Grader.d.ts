2262bd313c7adb61fa0bb6e7beb1fa63cf2653fd
IRONM00N <me@ironmoon.dev>
2025-08-03 19:48:40 -0400

    clean up dist/

--- dist/grader/Grader.d.ts
+++
@@ -1,2 +0,0 @@
-import { AutograderFeedback } from '../api/adminServiceSchemas.js';
-export default function grade(solutionDir: string, submissionDir: string, regressionTestJob?: number): Promise<AutograderFeedback>;
