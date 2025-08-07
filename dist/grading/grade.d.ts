0aa8d842382f8fc1c6982179d27f9a55abc9395e
IRONM00N <me@ironmoon.dev>
2025-08-07 17:24:44 -0400

    rm dist

--- dist/grading/grade.d.ts
+++
@@ -1,6 +0,0 @@
-import { AutograderFeedback } from '../api/adminServiceSchemas.js';
-import { PawtograderConfig } from './types.js';
-import { OverlayGrader } from './graders/OverlayGrader.js';
-import { PyretGrader } from './graders/PyretGrader.js';
-export declare function makeGrader(config: PawtograderConfig, solutionDir: string, submissionDir: string, regressionTestJob?: number): Promise<OverlayGrader | PyretGrader>;
-export default function grade(solutionDir: string, submissionDir: string, regressionTestJob?: number): Promise<AutograderFeedback>;
