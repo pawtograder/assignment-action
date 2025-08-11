409fe0367f5546a1968db949b532c8619efd1b19
IRONM00N <me@ironmoon.dev>
2025-08-11 13:38:01 -0400

    misc

--- nix/node-js-very-slim.nix
+++ nix/node-js-very-slim.nix
@@ -1,30 +1,27 @@
 { pkgs }:
-(pkgs.nodejs_24.override {
-  enableNpm = false;
-}).overrideAttrs
-  (old: {
-    doCheck = false;
+let
+  node-js-slim = pkgs.nodejs_24.override { enableNpm = false; };
+in
+pkgs.runCommand "nodejs-slim-stripped"
+  {
+    nativeBuildInputs = with pkgs; [ removeReferencesTo ];
 
-    outputs = [
-      "out"
-    ];
+    meta = {
+      mainProgram = "node";
+      description = "Node.js with dev references stripped";
+    };
 
-    # copied from nixpkgs; removed all v8 output
-    postInstall = ''
-      HOST_PATH=$out/bin patchShebangs --host $out
+    disallowedReferences = [ node-js-slim ];
+  }
+  ''
+    mkdir -p $out/bin
+    cp ${node-js-slim}/bin/node $out/bin/node
 
-      $out/bin/node --completion-bash > node.bash
-      installShellCompletion node.bash
+    remove-references-to -t ${pkgs.icu.dev} $out/bin/node
+    remove-references-to -t ${pkgs.openssl.dev} $out/bin/node
+    remove-references-to -t ${pkgs.sqlite.dev} $out/bin/node
+    remove-references-to -t ${pkgs.zlib.dev} $out/bin/node
+    remove-references-to -t ${pkgs.libuv.dev} $out/bin/node
 
-      rm -rf $out/include
-
-      rm -rf $out/lib/node_modules/corepack
-      rm -rf $out/bin/corepack
-
-      rm -rf $out/share/doc
-      rm -rf $out/share/man
-    '';
-
-    postConfigure = ""; # incorrectly assumes $dev will exist
-  })
-
+    remove-references-to -t ${node-js-slim} $out/bin/node
+  ''
