c7bf8dd3d1be0d63eecd2ddc19f0d8b0038fb423
IRONM00N <me@ironmoon.dev>
2025-08-11 22:05:38 -0400

    bump; improve flake

--- flake.nix
+++ flake.nix
@@ -12,8 +12,20 @@
       ...
     }:
     let
-      inherit (nixpkgs) lib;
-      eachSystem = f: lib.genAttrs (import systems) (s: f nixpkgs.legacyPackages.${s});
+      inherit (nixpkgs) lib legacyPackages;
+      eachSystem = f: lib.genAttrs (import systems) (system: f legacyPackages.${system});
+      mkCanvasNativeBuildInputs =
+        pkgs: with pkgs; [
+          gnumake
+          pkg-config
+          python3
+        ];
+      mkCanvasBuildInputs =
+        pkgs: with pkgs; [
+          pixman
+          cairo
+          pango
+        ];
     in
     {
       packages = eachSystem (pkgs: rec {
@@ -78,17 +90,8 @@
           dontNpmBuild = true;
           npmFlags = [ "--ignore-scripts" ];
 
-          nativeBuildInputs = with pkgs; [
-            gnumake
-            pkg-config
-            python3
-          ];
-
-          buildInputs = with pkgs; [
-            pixman
-            cairo
-            pango
-          ];
+          nativeBuildInputs = mkCanvasNativeBuildInputs pkgs;
+          buildInputs = mkCanvasBuildInputs pkgs;
 
           buildPhase = ''
             runHook preBuild
@@ -170,7 +173,7 @@
               cp ${pyret-lang-src}/package-lock.json $out/package-lock.json
             '';
           in
-          (pkgs.buildNpmPackage {
+          pkgs.buildNpmPackage {
             name = "pyret-runtime-deps";
             inherit src;
 
@@ -182,18 +185,8 @@
             npmPruneFlags = [ "--omit=dev" ];
             dontStrip = false;
 
-            nativeBuildInputs = with pkgs; [
-              gnumake
-              pkg-config
-              python3
-              removeReferencesTo
-            ];
-
-            buildInputs = with pkgs; [
-              pixman
-              cairo
-              pango
-            ];
+            nativeBuildInputs = (mkCanvasNativeBuildInputs pkgs) ++ [ pkgs.removeReferencesTo ];
+            buildInputs = mkCanvasBuildInputs pkgs;
 
             buildPhase = ''
               runHook preBuild
@@ -212,88 +205,76 @@
               find $out -type f -exec sed -i \
                 "s|${pkgs.nodejs_24}/bin/node|${node-js-very-slim}/bin/node|g" {} +
 
-              find $out -type f \( -name "*.node" -o -name "*.a" -o -name "*.json" \) \
-                -exec remove-references-to -t ${pkgs.nodejs_24.src} {} +
-
+              # HACK: these build artifacts reference node-src
               rm $out/node_modules/canvas/build/canvas.target.mk
               rm $out/node_modules/canvas/build/Makefile
               rm $out/node_modules/canvas/build/config.gypi
+              rm -rf $out/node_modules/canvas/build/Release/.deps
             '';
 
             disallowedReferences = with pkgs; [
               nodejs_24
               nodejs_24.src
             ];
-          });
+          };
 
-        action-build = (
-          pkgs.buildNpmPackage {
-            name = "pawtograder-assignment-action-build";
-            version = "1.0.0";
+        action-build = pkgs.buildNpmPackage {
+          name = "pawtograder-assignment-action-build";
+          version = "1.0.0";
 
-            src = lib.fileset.toSource {
-              root = ./.;
-              fileset = lib.fileset.unions [
-                ./src
-                ./pyret
-                ./package.json
-                ./package-lock.json
-                ./rollup.config.ts
-                ./tsconfig.json
-                ./tsconfig.base.json
-              ];
-            };
-
-            nodejs = pkgs.nodejs_24;
-            # npmDepsHash = lib.fakeHash;
-            npmDepsHash = "sha256-cKt+7luXpwPlZ0EM3yXJMnza1uuU8YTC3wo1FATPpMo=";
-            dontNpmBuild = true;
-            npmFlags = [ "--ignore-scripts" ];
-
-            nativeBuildInputs = with pkgs; [
-              gnumake
-              pkg-config
-              python3
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./src
+              ./pyret
+              ./package.json
+              ./package-lock.json
+              ./rollup.config.ts
+              ./tsconfig.json
+              ./tsconfig.base.json
             ];
+          };
 
-            buildInputs = with pkgs; [
-              pixman
-              cairo
-              pango
-            ];
+          nodejs = pkgs.nodejs_24;
+          # npmDepsHash = lib.fakeHash;
+          npmDepsHash = "sha256-1CYx+rscxPEZAGNZFDAJcATMKxvmp3/gVeSP4FpO9RA=";
+          dontNpmBuild = true;
+          npmFlags = [ "--ignore-scripts" ];
 
-            buildPhase = ''
-              runHook preBuild
+          nativeBuildInputs = mkCanvasNativeBuildInputs pkgs;
+          buildInputs = mkCanvasBuildInputs pkgs;
 
-              substituteInPlace node_modules/pyret-lang/Makefile \
-                --replace-fail "SHELL := /usr/bin/env bash" "SHELL := ${lib.getExe pkgs.bash}"
+          buildPhase = ''
+            runHook preBuild
 
-              npm rebuild
-              npm run package
+            substituteInPlace node_modules/pyret-lang/Makefile \
+              --replace-fail "SHELL := /usr/bin/env bash" "SHELL := ${lib.getExe pkgs.bash}"
 
-              npm exec --no pyret -- \
-                --builtin-js-dir ${pyret-lang-src}/src/js/trove/ \
-                --program pyret/main.arr \
-                --outfile pyret/main.cjs \
-                --no-check-mode --norun
+            npm rebuild
+            npm run package
 
-              runHook postBuild
-            '';
+            npm exec --no pyret -- \
+              --builtin-js-dir ${pyret-lang-src}/src/js/trove/ \
+              --program pyret/main.arr \
+              --outfile pyret/main.cjs \
+              --no-check-mode --norun
 
-            installPhase = ''
-              runHook preInstall
-              mkdir -p $out
-              cp -r dist $out/
-              cp pyret/main.cjs $out/
-              runHook postInstall
-            '';
+            runHook postBuild
+          '';
 
-            disallowedReferences = with pkgs; [
-              nodejs_24
-              nodejs_24.src
-            ];
-          }
-        );
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out
+            cp -r dist $out/
+            cp pyret/main.cjs $out/
+            runHook postInstall
+          '';
+
+          disallowedReferences = with pkgs; [
+            nodejs_24
+            nodejs_24.src
+          ];
+        };
 
         default =
           let
@@ -302,12 +283,6 @@
           pkgs.runCommand "pawtograder-assignment-action"
             {
               nativeBuildInputs = [ pkgs.makeWrapper ];
-
-              propagatedBuildInputs = with pkgs; [
-                cairo
-                pango
-                pixman
-              ];
             }
             ''
               mkdir -p $out/bin
