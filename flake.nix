d0ac48ae99c32b269ccb58a7286eb70e961613df
IRONM00N <me@ironmoon.dev>
2025-08-14 16:56:44 -0400

    bump: fixes guard lookup

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       inherit (nixpkgs) lib legacyPackages;
       eachSystem = f: lib.genAttrs (import systems) (system: f legacyPackages.${system});
       # mainNpmDepsHash = lib.fakeHash;
-      mainNpmDepsHash = "sha256-jZNjuDKjJC1zlwrC6LWAOvYknlJ844roWgA2wikrqlc=";
+      mainNpmDepsHash = "sha256-6alFVDaZBYXjloyx+gpYFfClBqxDbkeQxiFKRH/lhqo=";
     in
     {
       packages = eachSystem (pkgs: rec {
