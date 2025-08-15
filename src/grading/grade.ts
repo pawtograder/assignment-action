ecc73b6b435914d9847ab8a5775addae23a6fff7
IRONM00N <me@ironmoon.dev>
2025-08-14 21:42:04 -0400

    hardcode output

--- src/grading/grade.ts
+++ src/grading/grade.ts
@@ -52,15 +52,379 @@ export default async function grade(
   )
   const config = yaml.parse(_config) as PawtograderConfig
 
-  const grader = await makeGrader(
-    config,
-    solutionDir,
-    submissionDir,
-    regressionTestJob
-  )
-  const ret = await grader.grade()
+  // const grader = await makeGrader(
+  //   config,
+  //   solutionDir,
+  //   submissionDir,
+  //   regressionTestJob
+  // )
+  // const ret = await grader.grade()
 
-  console.log(JSON.stringify(ret, null, 0))
+  // console.log(JSON.stringify(ret, null, 0))
 
-  return ret
+  return {
+    tests: [
+      {
+        output_format: 'markdown',
+        output: 'Chaff for tree-map output',
+        hidden_output: 'Chaff for tree-map hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Chaff for tree-map',
+        max_score: 3,
+        score: 0,
+        part: 'tree-map'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Functional Test for tree-fold functional output',
+        hidden_output: 'Functional Test for tree-fold functional hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Functional Test for tree-fold functional',
+        max_score: 3,
+        score: 0,
+        part: 'tree-fold'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Chaff for tree-contains output',
+        hidden_output: 'Chaff for tree-contains hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Chaff for tree-contains',
+        max_score: 3,
+        score: 0,
+        part: 'tree-contains'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Chaff for tree-map output',
+        hidden_output: 'Chaff for tree-map hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Chaff for tree-map',
+        max_score: 3,
+        score: 0,
+        part: 'tree-map'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Self-Test on tree-sum output',
+        name: 'Self-Test on tree-sum',
+        max_score: 0,
+        score: 0,
+        part: 'tree-sum'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Chaff for tree-contains output',
+        hidden_output: 'Chaff for tree-contains hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Chaff for tree-contains',
+        max_score: 3,
+        score: 0,
+        part: 'tree-contains'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Functional Test for tree-contains functional output',
+        hidden_output:
+          'Functional Test for tree-contains functional hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Functional Test for tree-contains functional',
+        max_score: 3,
+        score: 0,
+        part: 'tree-contains'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Chaff for tree-fold output',
+        hidden_output: 'Chaff for tree-fold hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Chaff for tree-fold',
+        max_score: 3,
+        score: 0,
+        part: 'tree-fold'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Functional Test for tree-map functional output',
+        hidden_output: 'Functional Test for tree-map functional hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Functional Test for tree-map functional',
+        max_score: 3,
+        score: 0,
+        part: 'tree-map'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Chaff for tree-fold output',
+        hidden_output: 'Chaff for tree-fold hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Chaff for tree-fold',
+        max_score: 3,
+        score: 0,
+        part: 'tree-fold'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Wheat for tree-ormap output',
+        hidden_output: 'Wheat for tree-ormap hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Wheat for tree-ormap',
+        max_score: 3,
+        score: 0,
+        part: 'tree-ormap'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Chaff for tree-andmap output',
+        hidden_output: 'Chaff for tree-andmap hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Chaff for tree-andmap',
+        max_score: 3,
+        score: 0,
+        part: 'tree-andmap'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Wheat for tree-map output',
+        hidden_output: 'Wheat for tree-map hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Wheat for tree-map',
+        max_score: 3,
+        score: 0,
+        part: 'tree-map'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Chaff for tree-flatten output',
+        hidden_output: 'Chaff for tree-flatten hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Chaff for tree-flatten',
+        max_score: 3,
+        score: 0,
+        part: 'tree-flatten'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Wheat for tree-contains output',
+        hidden_output: 'Wheat for tree-contains hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Wheat for tree-contains',
+        max_score: 3,
+        score: 0,
+        part: 'tree-contains'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Chaff for tree-flatten output',
+        hidden_output: 'Chaff for tree-flatten hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Chaff for tree-flatten',
+        max_score: 3,
+        score: 0,
+        part: 'tree-flatten'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Chaff for tree-zero output',
+        hidden_output: 'Chaff for tree-zero hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Chaff for tree-zero',
+        max_score: 3,
+        score: 0,
+        part: 'tree-zero'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Wheat for tree-fold output',
+        hidden_output: 'Wheat for tree-fold hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Wheat for tree-fold',
+        max_score: 3,
+        score: 0,
+        part: 'tree-fold'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Wheat for tree-andmap output',
+        hidden_output: 'Wheat for tree-andmap hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Wheat for tree-andmap',
+        max_score: 3,
+        score: 0,
+        part: 'tree-andmap'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Wheat for tree-flatten output',
+        hidden_output: 'Wheat for tree-flatten hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Wheat for tree-flatten',
+        max_score: 3,
+        score: 0,
+        part: 'tree-flatten'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Chaff for tree-sum output',
+        hidden_output: 'Chaff for tree-sum hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Chaff for tree-sum',
+        max_score: 3,
+        score: 0,
+        part: 'tree-sum'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Self-Test on tree-ormap output',
+        name: 'Self-Test on tree-ormap',
+        max_score: 0,
+        score: 0,
+        part: 'tree-ormap'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Wheat for tree-zero output',
+        hidden_output: 'Wheat for tree-zero hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Wheat for tree-zero',
+        max_score: 3,
+        score: 0,
+        part: 'tree-zero'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Self-Test on tree-map output',
+        name: 'Self-Test on tree-map',
+        max_score: 0,
+        score: 0,
+        part: 'tree-map'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Functional Test for tree-sum functional output',
+        hidden_output: 'Functional Test for tree-sum functional hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Functional Test for tree-sum functional',
+        max_score: 3,
+        score: 0,
+        part: 'tree-sum'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Self-Test on tree-contains output',
+        name: 'Self-Test on tree-contains',
+        max_score: 0,
+        score: 0,
+        part: 'tree-contains'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Wheat for tree-sum output',
+        hidden_output: 'Wheat for tree-sum hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Wheat for tree-sum',
+        max_score: 3,
+        score: 0,
+        part: 'tree-sum'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Self-Test on tree-fold output',
+        name: 'Self-Test on tree-fold',
+        max_score: 0,
+        score: 0,
+        part: 'tree-fold'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Functional Test for tree-zero functional output',
+        hidden_output: 'Functional Test for tree-zero functional hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Functional Test for tree-zero functional',
+        max_score: 3,
+        score: 0,
+        part: 'tree-zero'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Self-Test on tree-andmap output',
+        name: 'Self-Test on tree-andmap',
+        max_score: 0,
+        score: 0,
+        part: 'tree-andmap'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Functional Test for tree-ormap functional output',
+        hidden_output:
+          'Functional Test for tree-ormap functional hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Functional Test for tree-ormap functional',
+        max_score: 3,
+        score: 0,
+        part: 'tree-ormap'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Self-Test on tree-flatten output',
+        name: 'Self-Test on tree-flatten',
+        max_score: 0,
+        score: 0,
+        part: 'tree-flatten'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Functional Test for tree-flatten functional output',
+        hidden_output:
+          'Functional Test for tree-flatten functional hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Functional Test for tree-flatten functional',
+        max_score: 3,
+        score: 0,
+        part: 'tree-flatten'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Functional Test for tree-andmap functional output',
+        hidden_output:
+          'Functional Test for tree-andmap functional hidden output',
+        hidden_output_format: 'markdown',
+        name: 'Functional Test for tree-andmap functional',
+        max_score: 3,
+        score: 0,
+        part: 'tree-andmap'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Self-Test on tree-zero output',
+        name: 'Self-Test on tree-zero',
+        max_score: 0,
+        score: 0,
+        part: 'tree-zero'
+      },
+      {
+        output_format: 'markdown',
+        output: 'Chaff for tree-ormap output',
+        hidden_output: '',
+        hidden_output_format: 'markdown',
+        name: 'Chaff for tree-ormap',
+        max_score: 3,
+        score: 0,
+        part: 'tree-ormap'
+      }
+    ],
+    output: {
+      visible: {
+        output_format: 'markdown',
+        output: 'visible output'
+      },
+      hidden: {
+        output_format: 'markdown',
+        output: 'hidden output'
+      }
+    },
+    lint: { status: 'pass', output: '' },
+    artifacts: [],
+    annotations: []
+  }
 }
